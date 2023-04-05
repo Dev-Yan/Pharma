ca24b209e4d1731faa7f554081df6dd7e6501310
Dev-Yan <yandevwork@gmail.com>
2023-04-04 21:01:22 -0300

    fix: endpoints

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { Category } from "../models/Category";
 import { CreateCategoryController } from "../controllers/category/CreateCategoryController";
 import { ListCategoriesController } from "../controllers/category/ListCategoryController";
 
 const routes = Router();
 
-routes.get("/category", new ListCategoriesController().execute);
+routes.get("/categories", new ListCategoriesController().execute);
 routes.post("/category", new CreateCategoryController().execute);
 
 export default routes;
